bb516e72c7e4e37e9fbe98793b9d2a297d123012
lana freitas <lanadeoliveira@gmail.com>
2022-11-14 13:49:14 -0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: 'layouts#application'
-  get 'ask', to: 'pages#ask'
   get 'answer', to: 'pages#answer'
+  get 'ask', to: 'pages#ask'
 end
